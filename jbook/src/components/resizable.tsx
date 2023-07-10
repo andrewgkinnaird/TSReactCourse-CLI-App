96dd6060e3ba10c4256c7fa65f92c0f8682b05c3
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2023-07-10 04:14:15 +0000

    added debouncing when bundling input code to preview pane

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import './resizable.css';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useEffect, useState } from 'react';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
@@ -9,20 +10,44 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight,setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width,setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const listener = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+    }
+  
+    window.addEventListener('resize',listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    }
+
+  },[]);
+
+  
+
 
   if (direction === 'horizontal') {
     resizableProps = {
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      className:'resize-horizontal',
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: width,
       resizeHandles: ['e'],
+      onResizeStop: (e, data) => {
+        setWidth(width);
+      },
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 24],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
-      height: 300,
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      height: innerHeight * 0.3,
       width: Infinity,
       resizeHandles: ['s'],
     };
